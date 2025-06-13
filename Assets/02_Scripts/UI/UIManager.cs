cd74cfb7d34cf378e0dc46ab534ca38af957525a
YBdhhh <97926god@gmail.com>
2025-06-13 10:34:02 +0900

    feat: 인게임창 UI

---
+++ Assets/02_Scripts/UI/UIManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null) instance = new UIManager();
+            return instance;
+        }
+    }
+    
+
+    public GameObject inGameUI;
+    public GameObject missionUI;    //list?
+    public GameObject meetingUI;
+    public GameObject votingUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowInGameUI()
+    {
+
+    }
+
+    public void ShowMissionUI()
+    {
+
+    }
+
+    public void ShowVotingUI()
+    {
+
+    }
+
+    public void ShowMeetingUI()
+    {
+
+    }
+}
