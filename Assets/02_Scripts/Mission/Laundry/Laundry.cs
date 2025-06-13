d09877105da96a3a10b0a23a9b7e2dca57673ba3
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-06-13 09:41:40 +0900

    feat: Laundry mission 서브 클래스 생성

---
+++ Assets/02_Scripts/Mission/Laundry/Laundry.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laundry : Mission
+{
+    private static readonly List<string> allCloths = new List<string>
+    {
+
+    };
+
+  
+
+    public override Mission Clone()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
