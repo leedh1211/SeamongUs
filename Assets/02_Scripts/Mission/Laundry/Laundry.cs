fe8881adc69b2af23931ac466be1facba0f977e6
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-06-13 10:32:14 +0900

    feature : Mission 서브클래스 laundry 작업 중간 업로드

--- Assets/02_Scripts/Mission/Laundry/Laundry.cs
+++ Assets/02_Scripts/Mission/Laundry/Laundry.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class Laundry : Mission
 {
-    private static readonly List<string> allCloths = new List<string>
-    {
+    //Resources/ClothPrefabs 폴더에 빨래 프리팹들 있음.
+    private List<GameObject> orderPrefabs;
+    private int currentIndex;
 
-    };
+    public Laundry() : base ("Laundry", "빨래를 제시된 순서대로 널어주세요.")
+    {
+        var prefabs = Resources.LoadAll<GameObject>("ClothPrefabs");
+        orderPrefabs = new List<GameObject>(prefabs);
+    }
 
   
 
     public override Mission Clone()
     {
-        throw new System.NotImplementedException();
+        var clone = new Laundry();
+        clone.orderPrefabs = new List<GameObject>(orderPrefabs);
+        clone.currentIndex = 0;
+        return clone;
     }
 
+    private void Shuffle<T>(List<T> list)
+    {
+        var rnd = new System.Random();
+        int n = list.Count;
+        while(n>1)
+        {
+            int k = rnd.Next(n--);
+            T temp = list[n];
+            list[n] = list[k];
+            list[k] = temp;
+        }
+    }
 }
