301d514d8801ca9adf2cac10dc4f434e2e2123e2
mcas0215 <kang33089343@gmail.com>
2025-06-15 07:07:51 +0900

    Add PlayerPropKey.cs, Fix NetWorkManager
    Add PlayrPropKey.cs, Fix NetWorkManager
    PlayerPropKey.cs to use playerData in external scripts

---
+++ Assets/02_Scripts/Network/PlayerPropKey.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class PlayerPropKey
+{
+    public const string Seq   = "SEQ";   // int
+    public const string Id    = "PID";   // string
+    public const string Nick  = "NICK";  // string
+    public const string Level = "LV";    // int
+    public const string Gold  = "GOLD";  // int
+    public const string Spr   = "SPR";   // string or int (스프라이트 인덱스)
+    public const string Dead  = "DEAD";  // bool
+    public const string Role  = "ROLE";  // byte
+}
