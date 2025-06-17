18ffac59c0389f7b1d4c0a927e641163e77e0d36
KUJ <thsusekdnlt1@naver.com>
2025-06-17 12:53:51 +0900

    feat : Intro 장면 추가 및 종료 시 LoginScene 전환
    Intro 장면 추가 및 종료 시 LoginScene 전환

---
+++ Assets/02_Scripts/Intro/IntroEnded.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroEnded : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SceneManager.LoadScene("Loginscene");
+    }
+}
---
+++ Assets/02_Scripts/Intro/IntroImageScroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroImageScroller : MonoBehaviour
+{
+    public float scrollSpeed = 2f;         // 이동 속도
+    public float endY = 262f;              // 최종 Y 좌표
+
+    private void Update()
+    {
+        if (transform.position.y < endY)
+        {
+            transform.position += new Vector3(0, scrollSpeed * Time.deltaTime, 0);
+        }
+    }
+}
