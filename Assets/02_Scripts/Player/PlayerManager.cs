7dfc461201ef8454931e712e19211137a97e6ca5
leedh1211 <ann959896@gmail.com>
2025-06-17 22:53:55 +0900

    fix : 병합중

--- Assets/02_Scripts/Player/PlayerManager.cs
+++ Assets/02_Scripts/Player/PlayerManager.cs
@@ -215,7 +215,8 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IOnEventCallback
                photonEvent.Code == EventCodes.PlayerAttacked ||
                photonEvent.Code == EventCodes.PlayerDied ||
                photonEvent.Code == EventCodes.VoteResult ||
-               photonEvent.Code == EventCodes.PlayerReport
+               photonEvent.Code == EventCodes.PlayerReport ||
+               photonEvent.Code == EventCodes.VoteResultKill
                ;
         
     }
