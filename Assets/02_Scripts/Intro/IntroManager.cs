a4e4c94adc0c47fde8dca6f377f49a547a2ef7a9
leedh1211 <ann959896@gmail.com>
2025-06-18 12:36:38 +0900

    fix : 인트로 매니저 사운드매니저 호출부분 시점변경

--- Assets/02_Scripts/Intro/IntroManager.cs
+++ Assets/02_Scripts/Intro/IntroManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class IntroManager : MonoBehaviour
 {
-    private void Awake()
+    private void Start()
     {
         SoundManager.Instance.PlayBGM(BGMType.Intro);
     }
